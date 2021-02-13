077c765589859a4d8b3a8aad02b81b71999cdb08
Dex Ilagan <Dex.Ilagan@didgigo.com.au>
2021-02-13 16:24:16 +0800

    added onclick events to add to list

---
+++ main.js
@@ -0,0 +1,22 @@
+let addToDoBtn = document.getElementById("addToDo");
+let collectionContainer = document.getElementById("collectionContainer");
+let inputField = document.getElementById("inputField");
+
+addToDoBtn.addEventListener('click', ()=>{
+  const inputValue = inputField.value;
+  if ( inputValue.trim() == '' ) return;
+
+
+  var listItem = document.createElement('li');
+  listItem.className = "collection-item";
+  listItem.innerHTML = `<div>${inputValue}
+                          <a href="#!" class="secondary-content">
+                          <i class="material-icons">remove</i>
+                        </a>
+                        </div>`;
+
+  collectionContainer.appendChild(listItem);
+
+
+
+});
