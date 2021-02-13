273a7613b5a04634d1eb0cccea12ebf84b957961
Dex Ilagan <Dex.Ilagan@didgigo.com.au>
2021-02-13 17:35:28 +0800

    Added validations and toast notifications

--- app.js
+++ app.js
@@ -32,13 +32,30 @@ class UI {
     listItem.className = "collection-item";
     listItem.innerHTML = `<div>${todo.title}
                             <a href="#!" class="secondary-content">
-                            <i class="material-icons">remove</i>
+                            <i class="material-icons removeList">remove</i>
                           </a>
                           </div>`;
     collectionContainer.appendChild(listItem);
 
   }
+
+  static clearFields(){
+    document.getElementById('inputField').value = "";
+  }
+
+  static validateField(fieldStringValue){
+    let message = "CANNOT ADD BLANK TODO!";
+    if ( fieldStringValue == '' || fieldStringValue.trim() == '')  
+    M.toast({html: message, classes : "red"});    
+  }
   
+  static deleteTodo(el){
+    if( el.classList.contains('removeList') ){
+      let parent = el.parentElement.parentElement.parentElement.remove();
+      console.log(parent);
+    }
+  
+  }
 
 }
 
@@ -48,3 +65,32 @@ class UI {
 
 //Event : Display TODOS
 document.addEventListener('DOMContentLoaded', UI.displayLists );
+
+
+//Event : Add a Book
+document.getElementById('addToDo')
+.addEventListener('click', (e)=>{
+  
+  const inputFieldValue = document.getElementById("inputField").value;
+
+  //Validate ToDo
+  UI.validateField( inputFieldValue )
+
+  //Intantiate Book
+  const newTodo = new ToDo(inputFieldValue);
+
+  //Add todo to UI
+  UI.addToDoToList({title : inputFieldValue});
+
+  //Clear fields
+  UI.clearFields();
+
+});
+
+
+//Event : Remove a Book
+ document.getElementById('collectionContainer')
+ .addEventListener('click', (e)=>{
+  UI.deleteTodo(e.target);
+
+ });
